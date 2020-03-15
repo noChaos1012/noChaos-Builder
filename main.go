970e1ab37a2a15934cbd6dd5b43dd37870f27e48
waschild1012 <waschild@163.com>
2020-03-15 16:06:30 +0800

    init beego framework

--- main.go
+++ main.go
@@ -1,2 +1,15 @@
 package main
-https://github.com/waschild1012/noChaos.git
+
+import (
+	_ "com.waschild/noChaos-Server/routers"
+
+	"github.com/astaxie/beego"
+)
+
+func main() {
+	if beego.BConfig.RunMode == "dev" {
+		beego.BConfig.WebConfig.DirectoryIndex = true
+		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+	}
+	beego.Run()
+}
